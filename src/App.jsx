eb1458d2088c1d84bd0d31a82ee16c4f4563e4f5
Abdullah-crns <abdullah2023037@gmail.com>
2025-01-25 15:13:43 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
-import Mainbody from './components/mainbody'
+import Mainbody from './components/MainBody'
 import './App.css'
 
 function App() {  
